0098bd28bb426ee7e6c635d7f3187c024e7c063e
Stanko <stanko.tadic@gmail.com>
2016-01-08 15:33:48 +0100

    Added audio support and moved timeline to the main JS file

--- js/canvas-video-player.js
+++ js/canvas-video-player.js
@@ -1,9 +1,19 @@
+var cvpHandlers = {
+	canvasClickHandler: null,
+	videoTimeUpdateHandler: null,
+	videoCanPlayHandler: null,
+	windowResizeHandler: null
+};
+
 var CanvasVideoPlayer = function(options) {
 	var i;
 
 	this.options = {
 		framesPerSecond: 25,
 		hideVideo: true,
+		autoplay: false,
+		audio: false,
+		timelineSelector: false,
 	};
 
 	for (i in options) {
@@ -12,6 +22,8 @@ var CanvasVideoPlayer = function(options) {
 
 	this.video = document.querySelectorAll(this.options.videoSelector)[0];
 	this.canvas = document.querySelectorAll(this.options.canvasSelector)[0];
+	this.timeline = document.querySelectorAll(this.options.timelineSelector)[0];
+	this.timelinePassed = document.querySelectorAll(this.options.timelineSelector + '> div')[0];
 
 	if (!this.options.videoSelector || !this.video) {
 		console.error('No "videoSelector" property, or the element is not found');
@@ -23,6 +35,32 @@ var CanvasVideoPlayer = function(options) {
 		return;
 	}
 
+	if (this.options.timelineSelector && !this.timeline) {
+		console.error('Element for the "timelineSelector" selector not found');
+		return;
+	}
+
+	if (this.options.timelineSelector && !this.timelinePassed) {
+		console.error('Element for the "timelinePassed" not found');
+		return;
+	}
+
+	if (this.options.audio) {
+		// Creates audio element which uses same video sources
+		this.audio = document.createElement('audio');
+		this.audio.innerHTML = this.video.innerHTML;
+		this.video.parentNode.insertBefore(this.audio, this.video);
+		this.audio.load();
+
+		var iOS = /iPad|iPhone|iPod/.test(navigator.platform);
+		if (iOS) {
+			// Autoplay doesn't work with audio on iOS
+			// User have to manually start the audio
+			this.options.autoplay = false;
+		}
+	}
+
+	// Canvas context
 	this.ctx = this.canvas.getContext('2d');
 
 	this.playing = false;
@@ -44,26 +82,72 @@ CanvasVideoPlayer.prototype.init = function() {
 	}
 };
 
+// Used most of the jQuery code for the .offset() method
+CanvasVideoPlayer.prototype.getOffset = function(elem) {
+	var docElem, rect, doc;
+
+	if (!elem) {
+		return;
+	}
+
+	rect = elem.getBoundingClientRect();
+
+	// Make sure element is not hidden (display: none) or disconnected
+	if (rect.width || rect.height || elem.getClientRects().length) {
+		doc = elem.ownerDocument;
+		docElem = doc.documentElement;
+
+		return {
+			top: rect.top + window.pageYOffset - docElem.clientTop,
+			left: rect.left + window.pageXOffset - docElem.clientLeft
+		};
+	}
+};
+
+CanvasVideoPlayer.prototype.jumpTo = function(percentage) {
+	this.video.currentTime = this.video.duration * percentage;
+
+	if (this.options.audio) {
+		this.audio.currentTime = this.audio.duration * percentage;
+	}
+};
+
 CanvasVideoPlayer.prototype.bind = function() {
 	var self = this;
 
 	// Playes or pauses video on canvas click
-	this.canvas.addEventListener('click', function canvasClickHandler() {
+	this.canvas.addEventListener('click', cvpHandlers.canvasClickHandler = function() {
 		self.playPause();
 	});
 
 	// On every time update draws frame
-	this.video.addEventListener('timeupdate', function videoTimeUpdateHandler() {
+	this.video.addEventListener('timeupdate', cvpHandlers.videoTimeUpdateHandler = function() {
 		self.drawFrame();
+		if (self.options.timelineSelector) {
+			self.updateTimeline();
+		}
 	});
 
 	// Draws first frame
-	this.video.addEventListener('canplay', function videoCanPlayHandler() {
+	this.video.addEventListener('canplay', cvpHandlers.videoCanPlayHandler = function() {
 		self.drawFrame();
 	});
 
+	if (self.options.autoplay) {
+	  self.play();
+	}
+
+	// Click on the video seek video
+	if (self.options.timelineSelector) {
+		this.timeline.addEventListener('click', function(e) {
+			var offset = e.clientX - self.getOffset(self.canvas).left;
+			var percentage = offset / self.timeline.offsetWidth;
+			self.jumpTo(percentage);
+		});
+	}
+
 	// Cache canvas size on resize (doing it only once in a second)
-	window.addEventListener('resize', function windowResizeHandler() {
+	window.addEventListener('resize', cvpHandlers.windowResizeHandler = function() {
 		clearTimeout(self.resizeTimeoutReference);
 
 		self.resizeTimeoutReference = setTimeout(function() {
@@ -73,14 +157,23 @@ CanvasVideoPlayer.prototype.bind = function() {
 	});
 
 	this.unbind = function() {
-		this.canvas.removeEventListener('click', canvasClickHandler);
-		this.video.removeEventListener('timeupdate', videoTimeUpdateHandler);
-		this.video.removeEventListener('canplay', videoCanPlayHandler);
-		window.removeEventListener('resize', windowResizeHandler);
+		this.canvas.removeEventListener('click', cvpHandlers.canvasClickHandler);
+		this.video.removeEventListener('timeupdate', cvpHandlers.videoTimeUpdateHandler);
+		this.video.removeEventListener('canplay', cvpHandlers.videoCanPlayHandler);
+		window.removeEventListener('resize', cvpHandlers.windowResizeHandler);
+
+		if (this.options.audio) {
+			this.audio.parentNode.removeChild(this.audio);
+		}
 	};
 };
 
-CanvasVideoPlayer.prototype.setCanvasSize = function(first_argument) {
+CanvasVideoPlayer.prototype.updateTimeline = function() {
+	var percentage = (this.video.currentTime * 100 / this.video.duration).toFixed(2);
+	this.timelinePassed.style.width = percentage + '%';
+};
+
+CanvasVideoPlayer.prototype.setCanvasSize = function() {
 	this.width = this.canvas.clientWidth;
 	this.height = this.canvas.clientHeight;
 
@@ -92,10 +185,18 @@ CanvasVideoPlayer.prototype.play = function() {
 	this.lastTime = Date.now();
 	this.playing = true;
 	this.loop();
+
+	if (this.options.audio) {
+		this.audio.play();
+	}
 };
 
 CanvasVideoPlayer.prototype.pause = function() {
 	this.playing = false;
+
+	if (this.options.audio) {
+		this.audio.pause();
+	}
 };
 
 CanvasVideoPlayer.prototype.playPause = function() {
@@ -122,6 +223,7 @@ CanvasVideoPlayer.prototype.loop = function() {
 	// If we are at the end of the video stop
 	if (this.video.currentTime >= this.video.duration) {
 		this.playing = false;
+		this.video.currentTime = 0;
 	}
 
 	if (this.playing) {
